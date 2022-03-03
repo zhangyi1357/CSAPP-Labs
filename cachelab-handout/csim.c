f234ab60542090d2b7e875c1584685acef314d5c
Zhang Yi <zhangyi_1357@sjtu.edu.cn>
2022-03-03 14:34:24 +0800

    read from command line using getopt

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,7 +1,42 @@
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "cachelab.h"
 
-int main()
-{
+void cache_simulation(int s, int E, int b, FILE* fp, bool verbose) {
+}
+
+int main(int argc, char* argv[]) {
+    int s, E, b;
+    bool verbose = false;
+    FILE* trace_fp;
+    char c;
+    while ((c = getopt(argc, argv, "s:E:b:t:v")) != -1) {
+        switch (c) {
+            case 's':
+                s = atoi(optarg);
+                break;
+            case 'E':
+                E = atoi(optarg);
+                break;
+            case 'b':
+                b = atoi(optarg);
+                break;
+            case 'v':
+                verbose = true;
+                break;
+            case 't':
+                trace_fp = fopen(optarg, "r");
+                break;
+            default:
+                printf("Illegall arg");
+                exit(1);
+        }
+    }
+    cache_simulation(s, E, b, trace_fp, verbose);
     printSummary(0, 0, 0);
     return 0;
 }
