d3552a69cb5b54ee9131811df6e8fff8458b83eb
Zhang Yi <zhangyi_1357@sjtu.edu.cn>
2022-03-03 20:21:31 +0800

    backup for valid bits

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,28 +1,90 @@
+#include <assert.h>
 #include <getopt.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "cachelab.h"
 
-#define MAXN 256
+#define MAXN 256  // buffer size
+#define M 32      // bits of memory address
 
 int hits = 0, misses = 0, evictions = 0;
 
 void cache_simulation(int s, int E, int b, FILE* fp, bool verbose) {
-    char c;
-    char buf[MAXN];
+    // malloc space for valid bits and set all bits to 0
+    size_t size_of_lines = (1 << s) * E;
+    size_t bytes_of_valid_bits = sizeof(char) * size_of_lines / 8 + 1;
+    char* valid_bits = malloc(bytes_of_valid_bits);
+    memset(valid_bits, 0, bytes_of_valid_bits);
+    printf("sizeof valid bits : %lu\n", sizeof(valid_bits));
+
+    // malloc space for flags
+    unsigned* flags = malloc(sizeof(unsigned) * size_of_lines);
+
+    // malloc space for lines frequency
+    size_t* hit_times = malloc(sizeof(size_t) * size_of_lines);
+    memset(hit_times, 0, sizeof(size_t) * size_of_lines);
+
+    // read the file and handle all the instructions
+    char buf[MAXN], c;
     while ((c = fgetc(fp)) != EOF) {
-        fgets(buf, MAXN, fp);
-        switch (c) {
-            case ' ':
-                break;
-            default:
-                continue;
+        if (c != ' ') {  // ignore the line starting without space (I line)
+            // skip this line
+            fgets(buf, MAXN, fp);
+            continue;
         }
-        printf("%s", buf);
+        // read the type and address
+        char type;
+        size_t address;
+        fscanf(fp, "%c %lx", &type, &address);
+        fgets(buf, MAXN, fp);  // skip useless ", size" left in the line
+        if (verbose)
+            printf("%c %lx%s", type, address, buf);
+
+        // decomposition of address
+        //      flag          set_index   offset
+        // bits M - (s + b)   s           b
+        size_t set_index = (address >> b) & ((1 << (s + 1)) - 1);
+        size_t flag = address >> (s + b);
+        size_t start_line_index = set_index * E;
+
+        size_t not_valid_bit_index = -1, i = 0;
+        for (i = 0; i < E; ++i) {
+            size_t index = start_line_index + i;
+            if (valid_bits[index / 8] & (1 << (index % 8))) {  // valid bit
+                if (flags[index] == flag)
+                    break;  // hit
+            } else {        // not valid bit
+                not_valid_bit_index = index;
+            }
+        }
+
+        if (i != E) {  // hit
+            ++hits;
+            if (verbose)
+                printf("hit");
+        } else if (not_valid_bit_index != -1) {  // empty line there
+            ++misses;
+            valid_bits[not_valid_bit_index / 8] |= (1 << (index % 8));
+            flags[not_valid_bit_index] = flag;
+            if (verbose)
+                printf("miss");
+        } else {  // eviction here
+            ++misses;
+            ++evictions;
+        }
+        if (type == M)
+            ++hits;
+
+        // printf("set index: %lx, flag: %lx\n", set_index, flag);
     }
+
+    free(hit_times);
+    free(valid_bits);
+    free(flags);
 }
 
 int main(int argc, char* argv[]) {
