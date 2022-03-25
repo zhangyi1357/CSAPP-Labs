ed1aa235f888355ec862004de337822b2ae71952
Zhang Yi <zhangyi_1357@sjtu.edu.cn>
2022-03-25 13:23:05 +0800

    change indent in code/netp

--- code/netp/echo.c
+++ code/netp/echo.c
@@ -1,19 +1,19 @@
 /*
  * echo - read and echo text lines until client closes connection
  */
-/* $begin echo */
+ /* $begin echo */
 #include "csapp.h"
 
-void echo(int connfd) 
+void echo(int connfd)
 {
-    size_t n; 
-    char buf[MAXLINE]; 
+    size_t n;
+    char buf[MAXLINE];
     rio_t rio;
 
     Rio_readinitb(&rio, connfd);
-    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { //line:netp:echo:eof
-	printf("server received %d bytes\n", (int)n);
-	Rio_writen(connfd, buf, n);
+    while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { //line:netp:echo:eof
+        printf("server received %d bytes\n", (int)n);
+        Rio_writen(connfd, buf, n);
     }
 }
 /* $end echo */
--- code/netp/netpfragments.c
+++ code/netp/netpfragments.c
@@ -23,17 +23,17 @@ void fragments()
         int              ai_family;    /* First arg to socket function */
         int              ai_socktype;  /* Second arg to socket function */
         int              ai_protocol;  /* Third arg to socket function  */
-        char            *ai_canonname; /* Canonical hostname */
+        char* ai_canonname; /* Canonical hostname */
         size_t           ai_addrlen;   /* Size of ai_addr struct */
-        struct sockaddr *ai_addr;      /* Ptr to socket address structure */
-        struct addrinfo *ai_next;      /* Ptr to next item in linked list */
+        struct sockaddr* ai_addr;      /* Ptr to socket address structure */
+        struct addrinfo* ai_next;      /* Ptr to next item in linked list */
     };
     /* $end addrinfo */
 
 
     /* $begin sockaddr */
     /* IP socket address structure */
-    struct sockaddr_in  {
+    struct sockaddr_in {
         uint16_t        sin_family;  /* Protocol family (always AF_INET) */
         uint16_t        sin_port;    /* Port number in network byte order */
         struct in_addr  sin_addr;    /* IP address in network byte order */
@@ -44,7 +44,7 @@ void fragments()
     struct sockaddr {
         uint16_t  sa_family;    /* Protocol family */
         char      sa_data[14];  /* Address data  */
-    };	
+    };
     /* $end sockaddr */
 
 }
